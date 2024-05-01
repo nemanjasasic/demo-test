72fce4b6e7e5ffac0b0824eaf3ad98e2e8e4ddee
nemanjasasic <snemanja99@gmail.com>
2024-05-01 17:30:44 +0200

    adding this python simple program

---
+++ test.py
@@ -0,0 +1,7 @@
+name = input("Your name ")
+day = input("which day ")
+hours = input("hours ")
+
+test = f"Hello how are you, yor name is {name}. Today is {day}. You are working this many horus {hours}"
+
+print (test)
